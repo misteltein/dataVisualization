f684793386f089d7729dd9616664d3a5312fb32c
misteltein <itogahibiki@gmail.com>
2020-10-19 14:11:59 +0900

    simplified

--- displayB3DPCA.py
+++ displayB3DPCA.py
@@ -1,8 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.decomposition import PCA
 
 data = np.loadtxt("dataA.csv",delimiter=',',skiprows=1)
+
+pca = PCA(n_components=3)
+pca.fit(data)
+
+print('principal component:')
+print(pca.components_)
+
+print('mean:')
+print(pca.mean_)
+
+transformedData = pca.transform(data)
 
 fig = plt.figure()
 ax = fig.add_subplot( projection = '3d' )
@@ -12,5 +24,5 @@ ax.set_zlabel('c')
 ax.set_xlim(-30,30)
 ax.set_ylim(-30,30)
 ax.set_zlim(-30,30)
-ax.scatter(data[:,0],data[:,1],data[:,2],alpha=0.05,s=1)
+ax.scatter(transformedData[:,0],transformedData[:,1],transformedData[:,2],alpha=0.05,s=1)
 plt.show()
