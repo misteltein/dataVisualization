5a99b900913462ba3eff6f078420213120b4beb9
misteltein <itogahibiki@gmail.com>
2020-10-19 14:32:54 +0900

    changed file name

--- displayA3Dwsl.py
+++ displayA3Dwsl.py
@@ -14,4 +14,6 @@ ax.set_ylim(-30,30)
 ax.set_zlim(-30,30)
 ax.view_init(elev=-160, azim=45)
 ax.scatter(data[:,0],data[:,1],data[:,2],alpha=0.05,s=1)
-plt.show()
+#plt.show()
+fig.savefig('dataA3D.png')
+
