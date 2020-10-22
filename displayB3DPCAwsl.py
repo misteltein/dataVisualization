8327df5d40c0a9a0ef6a7cd53831ec2a1a937761
misteltein <itogahibiki@gmail.com>
2020-10-23 05:28:55 +0900

    changed labels

--- displayB3DPCAwsl.py
+++ displayB3DPCAwsl.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
 
-data = np.loadtxt("dataA.csv",delimiter=',',skiprows=1)
+data = np.loadtxt("dataB.csv",delimiter=',',skiprows=1)
 
 pca = PCA(n_components=3)
 pca.fit(data)
@@ -25,9 +25,9 @@ transformedData = pca.transform(data)
 
 fig = plt.figure()
 ax = fig.add_subplot( projection = '3d' )
-ax.set_xlabel('a')
-ax.set_ylabel('b')
-ax.set_zlabel('c')
+ax.set_xlabel('PC1')
+ax.set_ylabel('PC2')
+ax.set_zlabel('PC3')
 ax.set_xlim(-30,30)
 ax.set_ylim(-30,30)
 ax.set_zlim(-30,30)
